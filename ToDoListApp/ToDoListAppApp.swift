96d2159251292835639448d22476f75954500427
ayanbaktygaliyev <ayan.baktygaliyev@gmail.com>
2023-09-28 01:47:11 +0600

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Ayan Baktygaliyev on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
